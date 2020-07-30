7ab537e23b266128cc5ad89c1cf5a58936a12d39
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2020-07-30 22:39:49 +0200

    tabs to spaces, for consistency sake

--- Scripts/Systems/LineSegmentRegisterSystem.cs
+++ Scripts/Systems/LineSegmentRegisterSystem.cs
@@ -23,42 +23,42 @@ namespace E7.ECS.LineRenderer
         Mesh _lineMesh;
 
         protected override void OnUpdate ()
-		{
+        {
             if( _lineMesh==null ) _lineMesh = CreateMesh();
-			var bounds = _lineMesh.bounds;
-			AABB aabb = new AABB{ Center=bounds.center , Extents = bounds.extents };
-			EntityCommandBuffer ecb = new EntityCommandBuffer( Allocator.Temp );
+            var bounds = _lineMesh.bounds;
+            AABB aabb = new AABB{ Center=bounds.center , Extents = bounds.extents };
+            EntityCommandBuffer ecb = new EntityCommandBuffer( Allocator.Temp );
 
-			Entities
-				.WithName("add_components_job")
-				.WithoutBurst()
-				.WithNone<RenderMesh>()
-				.ForEach( ( in Entity entity , in LineStyle style , in LineSegment segment ) =>
-				{
-					ecb.AddSharedComponent( entity , new RenderMesh{
-						mesh		= _lineMesh ,
-						material	= style.material
-					});
-					ecb.AddComponent( entity , ComponentType.ReadWrite<LocalToWorld>() );
-					ecb.AddComponent( entity , ComponentType.ReadWrite<RenderBounds>() );
-					ecb.SetComponent( entity , new RenderBounds{ Value = aabb });
-					ecb.AddComponent( entity , ComponentType.ReadWrite<WorldRenderBounds>() );
-				}).Run();
+            Entities
+                .WithName("add_components_job")
+                .WithoutBurst()
+                .WithNone<RenderMesh>()
+                .ForEach( ( in Entity entity , in LineStyle style , in LineSegment segment ) =>
+                {
+                    ecb.AddSharedComponent( entity , new RenderMesh{
+                        mesh        = _lineMesh ,
+                        material    = style.material
+                    });
+                    ecb.AddComponent( entity , ComponentType.ReadWrite<LocalToWorld>() );
+                    ecb.AddComponent( entity , ComponentType.ReadWrite<RenderBounds>() );
+                    ecb.SetComponent( entity , new RenderBounds{ Value = aabb });
+                    ecb.AddComponent( entity , ComponentType.ReadWrite<WorldRenderBounds>() );
+                }).Run();
 
-			ecb.Playback( EntityManager );
-			ecb.Dispose();
-		}
+            ecb.Playback( EntityManager );
+            ecb.Dispose();
+        }
 
         static Mesh CreateMesh ()
-		{
-			var mesh = new Mesh();
-			mesh.name = "quad 1x1, pivot at bottom center";
-			mesh.vertices = new Vector3[4]{ new Vector3{ x=-0.5f } , new Vector3{ x=0.5f } , new Vector3{ x=-0.5f , z=1 } , new Vector3{ x=0.5f , z=1 } };
-			mesh.triangles = new int[6]{ 0 , 2 , 1 , 2 , 3 , 1 };
-			mesh.normals = new Vector3[4]{ -Vector3.forward , -Vector3.forward , -Vector3.forward , -Vector3.forward };
-			mesh.uv = new Vector2[4]{ new Vector2{ x=0 , y=0 } , new Vector2{ x=1 , y=0 } , new Vector2{ x=0 , y=1 } , new Vector2{ x=1 , y=1 } };
-			return mesh;
-		}
+        {
+            var mesh = new Mesh();
+            mesh.name = "quad 1x1, pivot at bottom center";
+            mesh.vertices = new Vector3[4]{ new Vector3{ x=-0.5f } , new Vector3{ x=0.5f } , new Vector3{ x=-0.5f , z=1 } , new Vector3{ x=0.5f , z=1 } };
+            mesh.triangles = new int[6]{ 0 , 2 , 1 , 2 , 3 , 1 };
+            mesh.normals = new Vector3[4]{ -Vector3.forward , -Vector3.forward , -Vector3.forward , -Vector3.forward };
+            mesh.uv = new Vector2[4]{ new Vector2{ x=0 , y=0 } , new Vector2{ x=1 , y=0 } , new Vector2{ x=0 , y=1 } , new Vector2{ x=1 , y=1 } };
+            return mesh;
+        }
 
     }
 }
--- Scripts/Systems/LineSegmentTransformSystem.cs
+++ Scripts/Systems/LineSegmentTransformSystem.cs
@@ -14,39 +14,39 @@ namespace E7.ECS.LineRenderer
     public class LineSegmentTransformSystem : SystemBase
     {
         protected override void OnUpdate ()
-		{
-			Camera camera = Camera.main;
-			if( camera==null ) return;
-			Transform cameraTransform = camera.transform;
-			if( !camera.orthographic )// perspective-projection camera code path
-			{
-				float3 cameraPosition = cameraTransform.position;
-				Entities
-					.WithName("TRS_update_job")
-					.ForEach( ( ref LocalToWorld ltr , in LineSegment segment ) =>
-					{
-						float3 lineVec = segment.to - segment.from;
-						var rot = quaternion.LookRotation( math.normalize(lineVec) , math.normalize(cameraPosition-segment.from) );
-						var pos = segment.from;
-						var scale = new float3{ x=segment.lineWidth , y=1f , z=math.length(lineVec) };
-						ltr.Value = float4x4.TRS( pos , rot , scale );
-					}).ScheduleParallel();
-				}
-			else// orthographic-projection camera
-			{
-				quaternion cameraRotation = cameraTransform.rotation;
-				Entities
-					.WithName("TRS_orthographic_update_job")
-					.ForEach( ( ref LocalToWorld ltr , in LineSegment segment ) =>
-					{
-						float3 lineVec = segment.to - segment.from;
-						var rot = quaternion.LookRotation( math.normalize(lineVec) , math.mul(cameraRotation,new float3{z=-1}) );
-						var pos = segment.from;
-						var scale = new float3{ x=segment.lineWidth , y=1f , z=math.length(lineVec) };
-						ltr.Value = float4x4.TRS( pos , rot , scale );
-					}).ScheduleParallel();
-			}
-		}
+        {
+            Camera camera = Camera.main;
+            if( camera==null ) return;
+            Transform cameraTransform = camera.transform;
+            if( !camera.orthographic )// perspective-projection camera code path
+            {
+                float3 cameraPosition = cameraTransform.position;
+                Entities
+                    .WithName("TRS_update_job")
+                    .ForEach( ( ref LocalToWorld ltr , in LineSegment segment ) =>
+                    {
+                        float3 lineVec = segment.to - segment.from;
+                        var rot = quaternion.LookRotation( math.normalize(lineVec) , math.normalize(cameraPosition-segment.from) );
+                        var pos = segment.from;
+                        var scale = new float3{ x=segment.lineWidth , y=1f , z=math.length(lineVec) };
+                        ltr.Value = float4x4.TRS( pos , rot , scale );
+                    }).ScheduleParallel();
+                }
+            else// orthographic-projection camera
+            {
+                quaternion cameraRotation = cameraTransform.rotation;
+                Entities
+                    .WithName("TRS_orthographic_update_job")
+                    .ForEach( ( ref LocalToWorld ltr , in LineSegment segment ) =>
+                    {
+                        float3 lineVec = segment.to - segment.from;
+                        var rot = quaternion.LookRotation( math.normalize(lineVec) , math.mul(cameraRotation,new float3{z=-1}) );
+                        var pos = segment.from;
+                        var scale = new float3{ x=segment.lineWidth , y=1f , z=math.length(lineVec) };
+                        ltr.Value = float4x4.TRS( pos , rot , scale );
+                    }).ScheduleParallel();
+            }
+        }
 
-	}
+    }
 }
